00fdc9abf195cf8078a69a6ffc709f2f0a722e51
who i am <mrtevos17@gmail.com>
2022-02-03 13:49:33 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,28 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <link rel="shortcut icon" href="{{ asset("images/icon.png") }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/icon.png') }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aplikasi Legal Akses | JNE</title>
 
-    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
 <body>
     <div class="min-h-screen max-w-[1500px] mx-auto border">
         <header class="flex flex-col w-full">
             <div class="flex justify-between items-center p-4">
                 <a href="/">
-                    <img src="{{ asset("images/logo.png") }}" alt="Logo JNE" class="w-36">
+                    <img src="{{ asset('images/logo.png') }}" alt="Logo JNE" class="w-36">
                 </a>
+<<<<<<< HEAD
 
-                <a href="#" class="py-3 px-10 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Masuk</a>
+                <div class="flex justify-end">
+                    <div class="inline-flex rounded-md shadow-sm bg-gray-200" role="group">
+                        <a href="#"
+                            class="bg-transparent py-5 px-7 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            Tentang Kami
+                        </a>
+                        <a href="#"
+                            class="bg-transparent py-5 px-7 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            Layanan Hukum
+                        </a>
+                        <a href="#"
+                            class="bg-transparent py-5 px-7 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            Informasi
+                        </a>
+                        <a href="#"
+                            class="bg-transparent py-5 px-7 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            Database
+                        </a>
+                        <a href="/contact-us"
+                            class="bg-transparent py-5 px-7 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            Hubungi Kami
+                        </a>
+                        <a href="#"
+                            class="flex items-center justify-center text-center py-3 px-10 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Masuk</a>
+
+                    </div>
+=======
             </div>
 
             <div class="flex justify-end">
-                <div class="inline-flex rounded-md shadow-sm bg-gray-200" role="group">
+                <div class="inline-flex rounded-md shadow-sm bg-gray-200 -mt-16" role="group">
                     <a href="#" class="bg-transparent py-5 px-7 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
                         Tentang Kami
                     </a>
@@ -38,8 +69,13 @@
                     <a href="/contact-us" class="bg-transparent py-5 px-7 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
                         Hubungi Kami
                     </a>
+                    <a href="/contact-us" class="bg-transparent py-5 px-7 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                        Login
+                    </a>
+>>>>>>> 5e20864c92c0166d26f98639ab596d2e67f0d7ee
                 </div>
             </div>
+
 
 
             <div class="bg-[#D0391C] py-1 px-4">
@@ -47,7 +83,7 @@
                     @php
                         use Illuminate\Support\Carbon;
                     @endphp
-                    <span class="text-white"> {{ Carbon::now()->translatedFormat("l, d F Y | H:i") }} WIB</span>
+                    <span class="text-white"> {{ Carbon::now()->translatedFormat('l, d F Y | H:i') }} WIB</span>
                     <span class="text-white font-bold uppercase text-center">
                         Selamat Datang di Legal Service Access
                     </span>
@@ -60,4 +96,5 @@
         </main>
     </div>
 </body>
+
 </html>
